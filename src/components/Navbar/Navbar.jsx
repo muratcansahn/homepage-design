632addb459a14ecb1b7b3a48415485d1cc1909d9
Murat Can ŞAHİN <muratcansahin10@gmail.com>
2022-10-07 23:31:18 +0300

    add styled components

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,7 @@
+import { Nav } from "./styles";
+
+const Navbar = () => {
+  return <Nav>deneme</Nav>;
+};
+
+export default Navbar;
---
+++ src/components/Navbar/styles.js
@@ -0,0 +1,7 @@
+import styled from "styled-components";
+export const Nav = styled.nav`
+  color: #ecece;
+  background: #efefe;
+  font-size: 1.2rem;
+  font-weight: 700;
+`;
