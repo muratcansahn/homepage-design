a4fab6fcba810f0ebf294e8e9b902b75f772b273
Murat Can ŞAHİN <muratcansahin10@gmail.com>
2022-10-07 23:48:23 +0300

    create theme file

--- src/components/Navbar/styles.js
+++ src/components/Navbar/styles.js
@@ -1,6 +1,6 @@
 import styled from "styled-components";
 export const Nav = styled.nav`
-  color: #ecece;
+  color: ${(props) => props.theme.colors.yellow};
   background: #efefe;
   font-size: 1.2rem;
   font-weight: 700;
---
+++ src/theme.js
@@ -0,0 +1,13 @@
+export const theme = {
+  colors: {
+    black: "#232536",
+    yellow: "#FFD050",
+    yellowLight: "#FBF6EA",
+    yellowHover: "#EDC14A",
+    purple: "#592EA9",
+    greyDark: "#4C4C4C",
+    greyMedium: "#6D6E76",
+    greyLight: "#F4F4F4",
+    lavander: "#F4F0F8",
+  },
+};
